e0fb80599abb1265fec05c7d12c6c5937fe888c4
shreyash-malani <malanishreyash24@gmail.com>
2026-01-31 20:20:50 +0530

    One line added for practice

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Practicing Java programming! with github copilot");
-        System.out,println("This is a sample App class.i am doing this for pull request practice");
+        System.out.println("This is a sample App class.i am doing this for pull request practice");
+        System.out.println("Adding more lines to test git features.");
     }
 }
