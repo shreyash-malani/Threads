b8eba918ff8bbe020f0d12f710f7a18a73c93ec6
shreyash-malani <malanishreyash24@gmail.com>
2026-01-31 18:49:07 +0530

    Working on the rull request

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Practicing Java programming! with github copilot");
+        System.out,println("This is a sample App class.i am doing this for pull request practice");
     }
 }
